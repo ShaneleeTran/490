7e73eadcf45ee824d54b7d463c5221430b508a36
sojuljul <nguyenedge@yahoo.com>
2017-05-21 12:34:15 -0700

    Added wavedecoder

--- Assignement4Audio/Assignement4Audio/AudioFeatures.cs
+++ Assignement4Audio/Assignement4Audio/AudioFeatures.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Windows.Forms;
 using System.Collections;
 using Accord.Audio.Formats;
+using Accord.Audio;
 
 namespace Assignement4Audio
 {
@@ -16,7 +17,21 @@ namespace Assignement4Audio
 
         public void SetDirectoryPaths(string folderPath)
         {
-            audioFileName = Directory.GetFiles(folderPath, "*.wav").Select(Path.GetFileName).ToArray();
+            audioFileName = Directory.GetFiles(folderPath, "*.wav").Select(Path.GetFileName).OrderBy(r => r.Length).ToArray();
+
+            for (int i = 0; i < audioFileName.Length; i++)
+            {
+                string fileName = audioFileName[i];
+                WaveDecoder sourceWD = new WaveDecoder(fileName);
+                Signal sourceS = sourceWD.Decode();
+
+                //RaisedCosineWindow window = RaisedCosineWindow.Hamming(1024);
+                //Signal[] windows = sourceS.Split(window, 512);
+
+                //ComplexSignal[] complex = windows.Apply(ComplexSignal.FromSignal);
+
+                //complex.ForwardFourierTransform();
+            }
         }
     }
 }
